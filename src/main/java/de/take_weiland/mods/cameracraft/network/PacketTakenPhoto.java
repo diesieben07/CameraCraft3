cf22846f734ce03d942b067d62944693f3e301ff
diesieben07 <diesieben07@gmail.com>
2014-01-07 16:26:15 +0100

    Cleanup old code in the photo transfer packet

--- src/main/java/de/take_weiland/mods/cameracraft/network/PacketTakenPhoto.java
+++ src/main/java/de/take_weiland/mods/cameracraft/network/PacketTakenPhoto.java
@@ -3,28 +3,15 @@ package de.take_weiland.mods.cameracraft.network;
 import java.awt.image.BufferedImage;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 import javax.imageio.ImageIO;
 
-import net.minecraft.crash.CrashReport;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.ChatMessageComponent;
-import net.minecraft.util.EnumChatFormatting;
-import net.minecraft.util.ReportedException;
-
-import com.google.common.io.ByteStreams;
-
 import cpw.mods.fml.relauncher.Side;
 import de.take_weiland.mods.cameracraft.PhotoRequestManager;
-import de.take_weiland.mods.cameracraft.api.img.ImageFilter;
 import de.take_weiland.mods.commons.network.MultipartDataPacket;
 import de.take_weiland.mods.commons.network.PacketType;
-import de.take_weiland.mods.commons.util.Scheduler;
 
 public class PacketTakenPhoto extends MultipartDataPacket {
 
@@ -52,67 +39,6 @@ public class PacketTakenPhoto extends MultipartDataPacket {
 		transferId = in.readInt();
 		image = ImageIO.read(in);
 		PhotoRequestManager.incomingPhoto(player, transferId, image);
-		
-//		
-//		ItemStack currentStack = player.getCurrentEquippedItem();
-//		Item cameraItem;
-//		if (currentStack == null || !((cameraItem = currentStack.getItem()) instanceof CameraItem)) {
-//			warnHack(player);
-//		} else {
-//			Camera camera = ((CameraItem)cameraItem).newCamera(player);
-//			if (camera.hasStorage()) {
-//				PhotoStorage storage = camera.getPhotoStorage();
-//				
-//				String photoId = CCWorldData.get(player.worldObj).nextId();
-//				int idx = storage.store(photoId);
-//				ImageFilter filter = camera.getFilter();
-//				camera.dispose();
-//				
-//				if (idx < 0) {
-//					warnHack(player);
-//				} else {
-//					File file = PhotoManager.getImageFile(photoId);
-//					CameraCraft.executor.execute(new ImageDataSaver(photoId, in, file, filter));
-//				}
-//			} else {
-//				warnHack(player);
-//			}
-//		}
-	}
-
-	private static void warnHack(EntityPlayer player) {
-		player.sendChatToPlayer(ChatMessageComponent.createFromText("Hey you! Don't hack!").setColor(EnumChatFormatting.DARK_RED));
-	}
-	
-	private static final class ImageDataSaver implements Runnable {
-		final String photoId;
-		private final InputStream in;
-		private final File file;
-		private final ImageFilter filter;
-
-		ImageDataSaver(String photoId, InputStream in, File file, ImageFilter filter) {
-			this.photoId = photoId;
-			this.in = in;
-			this.file = file;
-			this.filter = filter;
-		}
-
-		@Override
-		public void run() {
-			try {
-				if (filter != null) {
-					ImageIO.write(filter.apply(ImageIO.read(in)), "PNG", file);
-				} else {
-					OutputStream out = new FileOutputStream(file);
-					ByteStreams.copy(in, out);
-					out.close();
-				}
-			} catch (IOException e) {
-				CrashReport cr = CrashReport.makeCrashReport(e, "Saving CameraCraft Image file");
-				cr.makeCategory("Photo being saved").addCrashSection("PhotoId", photoId);
-				Scheduler.server().throwInMainThread(new ReportedException(cr));
-			}
-		}
 	}
 
 	@Override
