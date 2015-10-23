0c424d963d6aa606d2e364fe4c830e7f3358a2c4
Take Weiland <diesieben07@gmail.com>
2015-10-23 23:37:08 +0200

    Fix TileScanner

--- src/main/java/de/take_weiland/mods/cameracraft/tileentity/TileScanner.java
+++ src/main/java/de/take_weiland/mods/cameracraft/tileentity/TileScanner.java
@@ -1,21 +1,17 @@
 package de.take_weiland.mods.cameracraft.tileentity;
 
 import de.take_weiland.mods.cameracraft.api.PhotoStorageProvider;
-import de.take_weiland.mods.cameracraft.api.cable.NetworkNode;
-import de.take_weiland.mods.cameracraft.api.cable.NetworkTile;
 import de.take_weiland.mods.cameracraft.api.photo.PhotoStorage;
 import de.take_weiland.mods.cameracraft.api.photo.PhotoStorageItem;
+import de.take_weiland.mods.cameracraft.blocks.CCBlock;
 import de.take_weiland.mods.cameracraft.blocks.MachineType;
-import de.take_weiland.mods.cameracraft.networking.NetworkNodeImpl;
+import de.take_weiland.mods.commons.meta.HasSubtypes;
 import de.take_weiland.mods.commons.tileentity.TileEntityInventory;
-import de.take_weiland.mods.commons.util.Multitypes;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
-public class TileScanner extends TileEntityInventory<TileScanner> implements NetworkTile, PhotoStorageProvider {
+public class TileScanner extends TileEntityInventory implements PhotoStorageProvider {
 
-	private final NetworkNodeImpl node = new NetworkNodeImpl(this);
-	
 	@Override
 	public int getSizeInventory() {
 		return 1;
@@ -28,8 +24,8 @@ public class TileScanner extends TileEntityInventory<TileScanner> implements Net
 	}
 
 	@Override
-	protected String unlocalizedName() {
-		return Multitypes.fullName(MachineType.SCANNER);
+	public String getDefaultName() {
+		return HasSubtypes.name(CCBlock.machines, MachineType.SCANNER);
 	}
 
 	@Override
@@ -41,28 +37,6 @@ public class TileScanner extends TileEntityInventory<TileScanner> implements Net
 	private ItemStack getPhotoStorageItem() {
 		ItemStack item = storage[0];
 		return item != null && item.getItem() instanceof PhotoStorageItem ? item : null;
-	}
-
-	@Override
-	public String getNetworkName() {
-		return "Scanner [x=" + xCoord + ", y=" + yCoord + ", z=" + zCoord + "]";
-	}
-
-	@Override
-	public NetworkNode getNode() {
-		return node;
-	}
-
-	@Override
-	public void updateEntity() {
-		super.updateEntity();
-		node.update();
-	}
-
-	@Override
-	public void invalidate() {
-		super.invalidate();
-		node.shutdown();
 	}
 
 }
