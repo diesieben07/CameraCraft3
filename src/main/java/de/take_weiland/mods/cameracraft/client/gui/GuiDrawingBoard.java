5a045f724874a7a3995b45b58c71531ca6dbd565
Kloon <geilomat4@gmail.com>
2015-11-01 14:39:31 +0100

    Added modifiable Dynamic Texture + some stuff in Drawinf Table
    #No_rip_for_NiP

--- src/main/java/de/take_weiland/mods/cameracraft/client/gui/GuiDrawingBoard.java
+++ src/main/java/de/take_weiland/mods/cameracraft/client/gui/GuiDrawingBoard.java
@@ -4,6 +4,7 @@ import de.take_weiland.mods.cameracraft.api.photo.PhotoItem;
 import de.take_weiland.mods.cameracraft.client.PhotoDataCache;
 import de.take_weiland.mods.cameracraft.client.gui.state.GuiContainerGuiState;
 import de.take_weiland.mods.cameracraft.client.gui.state.GuiStateContainer;
+import de.take_weiland.mods.cameracraft.client.texture.DynTexture;
 import de.take_weiland.mods.cameracraft.gui.ContainerDrawingBoard;
 import de.take_weiland.mods.cameracraft.item.ItemPhoto;
 import de.take_weiland.mods.commons.client.Guis;
@@ -12,6 +13,7 @@ import net.minecraft.client.gui.GuiButton;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -19,15 +21,15 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
-import static org.lwjgl.opengl.GL11.glColor3f;
-
 /**
  * @author Intektor
  */
 public class GuiDrawingBoard extends GuiContainerGuiState<ContainerDrawingBoard> {
 
     protected BufferedImage image;
+    protected DynTexture texture;
     protected final int scale = 8;
+    private int red, blue, green;
 
     public GuiDrawingBoard(ContainerDrawingBoard container) {
         super(container);
@@ -37,7 +39,20 @@ public class GuiDrawingBoard extends GuiContainerGuiState<ContainerDrawingBoard>
     public void initGui() {
         super.initGui();
         guiStates.add(new GuiStateContainer(container, this, new ResourceLocation("cameracraft:textures/gui/scanner.png"), new int[]{0}, new GuiButton[]{new GuiButton(0, width / 2 - 60, height / 2 - guiTop / 2 - 5, 120, 20, "Use this for drawing")}, true, guiTop, guiLeft));
-        guiStates.add(new GuiStateContainer(container, this, null, new int[]{1}, new GuiButton[]{}, false, 0, 0));
+
+        GuiButton[] buttons1 = new GuiButton[]{
+                new GuiButton(0, 0, 20, 75, 20, "Finish Drawing"),
+                new GuiButton(1, width - 90, 5, 20, 20, "-"),
+                new GuiButton(2, width - 25, 5, 20, 20, "+"),
+
+                new GuiButton(3, width - 90, 30, 20, 20, "-"),
+                new GuiButton(4, width - 25, 30, 20, 20, "+"),
+
+                new GuiButton(5, width - 90, 55, 20, 20, "-"),
+                new GuiButton(6, width - 25, 55, 20, 20, "+"),
+        };
+
+        guiStates.add(new GuiStateContainer(container, this, null, new int[]{1}, buttons1, false, 0, 0));
         initGuiState();
     }
 
@@ -45,6 +60,19 @@ public class GuiDrawingBoard extends GuiContainerGuiState<ContainerDrawingBoard>
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         super.drawScreen(mouseX, mouseY, partialTicks);
         if (getActiveGuiStateNumber() == 1) {
+
+            drawCenteredString(mc.fontRendererObj, "Red: " + red, ((width - 90) + (width - 25)) / 2 + 10, 5 + 6, Color.red.getRGB());
+            drawCenteredString(mc.fontRendererObj, "Green: " + green, ((width - 90) + (width - 25)) / 2 + 10, 30 + 6, Color.green.getRGB());
+            drawCenteredString(mc.fontRendererObj, "Blue: " + blue, ((width - 90) + (width - 25)) / 2 + 10, 55 + 6, Color.blue.getRGB());
+
+            int red2 = ((int) (red * 25.5) << 16) & 0x00FF0000;
+            int green2 = ((int) (green * 25.5) << 8) & 0x0000FF00;
+            int blue2 = (int) (blue * 25.5) & 0x000000FF;
+            int color = 0xFF000000 | red2 | green2 | blue2;
+
+
+            Rendering.drawColoredQuad(((width - 90) + (width - 25)) / 2 + 2, 80, 15, 15, color);
+
             int size = Math.min(height, width) - 4;
 
             int x = (width - size) / 2;
@@ -53,7 +81,10 @@ public class GuiDrawingBoard extends GuiContainerGuiState<ContainerDrawingBoard>
             drawRect(x, y, x + size, y + size, Color.black.getRGB());
             ItemPhoto photo = (ItemPhoto) container.getSlot(1).getStack().getItem();
             PhotoDataCache.bindTexture(photo.getPhotoId(container.getSlot(1).getStack()));
-            glColor3f(1, 1, 1);
+            GL11.glColor3f(1, 1, 1);
+            Rendering.drawTexturedQuadFit(x + 2, y + 2, size - 4, size - 4);
+
+            mc.renderEngine.bindTexture(mc.renderEngine.getDynamicTextureLocation("camera.craft", texture));
             Rendering.drawTexturedQuadFit(x + 2, y + 2, size - 4, size - 4);
         }
     }
@@ -107,6 +138,43 @@ public class GuiDrawingBoard extends GuiContainerGuiState<ContainerDrawingBoard>
                 setGuiState(1);
             }
         }
+        if (activeGuiState == 1) {
+            switch (button.id) {
+                case 0:
+                    setGuiState(0);
+                    break;
+                case 1:
+                    if (red > 0) {
+                        red--;
+                    }
+                    break;
+                case 2:
+                    if (red < 10) {
+                        red++;
+                    }
+                    break;
+                case 3:
+                    if (green > 0) {
+                        green--;
+                    }
+                    break;
+                case 4:
+                    if (green < 10) {
+                        green++;
+                    }
+                    break;
+                case 5:
+                    if (blue > 0) {
+                        blue--;
+                    }
+                    break;
+                case 6:
+                    if (blue < 10) {
+                        blue++;
+                    }
+                    break;
+            }
+        }
     }
 
     @Override
@@ -122,7 +190,7 @@ public class GuiDrawingBoard extends GuiContainerGuiState<ContainerDrawingBoard>
 
     protected void draw(int mouseX, int mouseY, int mouseButton) {
         int size = Math.min(height, width) - 4;
-        int x = (width + 4 - size) / 2 ;
+        int x = (width + 4 - size) / 2;
         int y = (height + 4 - size) / 2;
         int resolution = 256;
 
@@ -132,7 +200,16 @@ public class GuiDrawingBoard extends GuiContainerGuiState<ContainerDrawingBoard>
             int pixelX = mouseXinFrame % resolution;
             int pixelY = mouseYinFrame % resolution;
 
-            System.out.println(pixelX + "\t" + pixelY);
+            int red2 = ((int) (red * 25.5) << 16) & 0x00FF0000;
+            int green2 = ((int) (green * 25.5) << 8) & 0x0000FF00;
+            int blue2 = (int) (blue * 25.5) & 0x000000FF;
+            int color = 0xFF000000 | red2 | green2 | blue2;
+
+            image.setRGB(pixelX, pixelY, Color.black.getRGB());
+
+            texture.updateBufferedImage(image);
+
+            System.out.println(pixelX + "\t" + pixelY + "\t" + image.getRGB(pixelX, pixelY));
         }
     }
 
@@ -151,12 +228,15 @@ public class GuiDrawingBoard extends GuiContainerGuiState<ContainerDrawingBoard>
                 }
             } else {
                 int sizeZ = Math.min(height, width) - 4;
-                if(size.getWidth() != 0) {
+                if (size.getWidth() != 0) {
                     image = new BufferedImage(size.getWidth(), size.getHeight(), BufferedImage.TYPE_INT_ARGB);
-                }else{
+                } else {
                     image = new BufferedImage(256, 256, BufferedImage.TYPE_INT_ARGB);
                 }
             }
+            texture = new DynTexture(image);
         }
     }
+
+
 }
---
+++ src/main/java/de/take_weiland/mods/cameracraft/client/texture/DynTexture.java
@@ -0,0 +1,44 @@
+package de.take_weiland.mods.cameracraft.client.texture;
+
+import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.client.renderer.texture.TextureUtil;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * @author Intektor
+ */
+public class DynTexture extends DynamicTexture {
+
+    protected int width, height;
+    protected int[] textureData;
+
+    public DynTexture(BufferedImage img) {
+        this(img.getWidth(), img.getHeight());
+        img.getRGB(0, 0, img.getWidth(), img.getHeight(), textureData, 0, img.getWidth());
+        this.updateDynamicTexture();
+    }
+
+    public DynTexture(int width, int height) {
+        super(width, height);
+        this.width = width;
+        this.height = height;
+        this.textureData = new int[width * height];
+        TextureUtil.allocateTexture(this.getGlTextureId(), width, height);
+    }
+
+    @Override
+    public void updateDynamicTexture() {
+        TextureUtil.uploadTexture(this.getGlTextureId(), textureData, width, height);
+    }
+
+    @Override
+    public int[] getTextureData() {
+        return textureData;
+    }
+
+    public void updateBufferedImage(BufferedImage img) {
+        img.getRGB(0, 0, img.getWidth(), img.getHeight(), textureData, 0, img.getWidth());
+        this.updateDynamicTexture();
+    }
+}
