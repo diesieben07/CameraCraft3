ef7f00e45add9ffcfdfc6a50bbde5c674b188e12
Take Weiland <diesieben07@gmail.com>
2015-11-03 17:31:07 +0100

    Improve overexpose filter

--- src/main/java/de/take_weiland/mods/cameracraft/img/OverexposeFilter.java
+++ src/main/java/de/take_weiland/mods/cameracraft/img/OverexposeFilter.java
@@ -1,23 +1,28 @@
 package de.take_weiland.mods.cameracraft.img;
 
-import static de.take_weiland.mods.cameracraft.img.ImageUtil.clampRgb;
 import de.take_weiland.mods.cameracraft.api.img.SimpleRgbFilter;
+
+import static de.take_weiland.mods.cameracraft.img.ImageUtil.clampRgb;
 
 class OverexposeFilter implements SimpleRgbFilter {
 
-	private static final int FACTOR = 14;
+	private static final int FACTOR = 12;
 
 	@Override
 	public int apply(int rgb) {
-		int r = (rgb >> 16) & 0xff;
-    	int g = (rgb >> 8) & 0xff;
+		int r = rgb >> 16 & 0xff;
+    	int g = rgb >> 8 & 0xff;
     	int b = rgb & 0xff;
     	
-    	r = clampRgb(r * FACTOR);
-    	g = clampRgb(g * FACTOR);
-    	b = clampRgb(b * FACTOR);
-    	
-    	return r << 16 | g << 8 | b;
+    	r = modRGB(r);
+    	g = modRGB(g);
+        b = modRGB(b);
+
+        return r << 16 | g << 8 | b;
 	}
+
+    private static int modRGB(int v) {
+        return clampRgb(Math.max(v * FACTOR, 200));
+    }
 
 }
