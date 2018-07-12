18d4126b46497171529cbe621d2ad9c61048c465
Take Weiland <diesieben07@gmail.com>
2018-07-13 00:27:46 +0100

    Update registration

--- src/main/kotlin/de/takeweiland/mods/cameracraft/items/Registration.kt
+++ src/main/kotlin/de/takeweiland/mods/cameracraft/items/Registration.kt
@@ -2,6 +2,7 @@
 package de.takeweiland.mods.cameracraft.items
 
 import de.takeweiland.mods.cameracraft.CC_CREATIVE_TAB
+import de.takeweiland.mods.cameracraft.CC_MODID
 import net.minecraft.client.renderer.block.model.ModelResourceLocation
 import net.minecraft.item.Item
 import net.minecraftforge.client.event.ModelRegistryEvent
@@ -9,6 +10,7 @@ import net.minecraftforge.client.model.ModelLoader
 import net.minecraftforge.event.RegistryEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.registry.ForgeRegistries
 import net.minecraftforge.fml.relauncher.Side
 import net.minecraftforge.fml.relauncher.SideOnly
 
@@ -33,5 +35,10 @@ internal fun registerItems(event: RegistryEvent.Register<Item>) {
 @SideOnly(Side.CLIENT)
 @SubscribeEvent
 internal fun registerModels(event: ModelRegistryEvent) {
-    ModelLoader.setCustomModelResourceLocation(CAMERA, 0, ModelResourceLocation(CAMERA.registryName!!, "inventory"))
+    for (item in ForgeRegistries.ITEMS) {
+        if (item.registryName!!.resourceDomain == CC_MODID) {
+            ModelLoader.setCustomModelResourceLocation(item, 0, ModelResourceLocation(item.registryName!!, "inventory"))
+        }
+    }
+
 }
